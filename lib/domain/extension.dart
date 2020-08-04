579e5fd031d255f7b60b5973b08a4de088ec13a4
huwentao <hu.wentao@outlook.com>
2020-08-04 10:32:54 +0800

    add(extension.dart): 允许Failure, Future<Failure>快速调用 IDialog.err()方法创建对话框

---
+++ lib/domain/extension.dart
@@ -0,0 +1,21 @@
+// Created by Hu Wentao.
+// Email : hu.wentao@outlook.com
+// Date  : 2020/8/4
+// Time  : 1:02
+
+import 'package:flutter/widgets.dart';
+import 'package:get_arch_quick_start/qs_interface.dart';
+
+///
+/// 用于View中快速调用 Dialog.err()
+extension DialogX on Failure {
+  errDialog(BuildContext ctx, {dynamic tag, String instanceName}) {
+    GetIt.I<IDialog>(instanceName: instanceName).err(this, ctx: ctx, tag: tag);
+  }
+}
+
+extension FutureDialogX on Future<Failure> {
+  errDialog(BuildContext ctx, {dynamic tag, String instanceName}) async {
+    (await this).errDialog(ctx, tag: tag, instanceName: instanceName);
+  }
+}
