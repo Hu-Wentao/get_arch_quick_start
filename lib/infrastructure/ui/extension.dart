1cc4d498c3112fb18a82fd15dd136c8ba6efa233
huwentao <hu.wentao@outlook.com>
2020-11-01 18:16:29 +0800

    feat(extension): 为DialogX新增mapFailure(); errDialog()新增返回值泛型; 为FutureDialogX新增asyncMapFailure,asyncErrDialog新增返回值泛型

--- lib/infrastructure/ui/extension.dart
+++ lib/infrastructure/ui/extension.dart
@@ -3,6 +3,7 @@
 // Date  : 2020/8/4
 // Time  : 1:02
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get_arch_quick_start/qs_domain.dart';
 import 'package:get_arch_quick_start/qs_interface.dart';
@@ -11,8 +12,15 @@ import 'package:get_arch_quick_start/qs_interface.dart';
 /// 用于View中快速调用 Dialog.err()
 extension DialogX on Failure {
   ///
+  /// 预处理Failure, 可以放在 [errDialog]之前
+  /// ```dart
+  /// foo().onFailure((f){ ..do sth... return xxx}).errDialog(context);
+  /// ```
+  Failure mapFailure(Failure Function(Failure f) mapFailure) => mapFailure(this);
+
+  ///
   /// [T] 通常情况下为 Widget, 如果有需要的话
-  errDialog<T>(BuildContext ctx,
+  T errDialog<T>(BuildContext ctx,
       {dynamic tag, String instanceName, IDialog dialog, T returnVal}) {
     if (this != null)
       (dialog ?? GetIt.I<IDialog>(instanceName: instanceName))
@@ -22,7 +30,10 @@ extension DialogX on Failure {
 }
 
 extension FutureDialogX on Future<Failure> {
-  asyncErrDialog<T>(BuildContext ctx,
+  Future<Failure> asyncMapFailure(Failure Function(Failure f) mapFailure) =>
+      this?.then(mapFailure);
+
+  Future<T> asyncErrDialog<T>(BuildContext ctx,
       {dynamic tag, String instanceName, IDialog dialog, T returnVal}) async {
     this?.then((value) => value.errDialog(ctx,
         tag: tag,
@@ -31,4 +42,12 @@ extension FutureDialogX on Future<Failure> {
         returnVal: returnVal));
     return returnVal;
   }
+}
+
+///
+/// BuildContext扩展
+extension BuildContextX on BuildContext {
+  MediaQueryData get query => MediaQuery.of(this);
+
+  ThemeData get themeData => Theme.of(this);
 }
